9a9b3e5a97219cfe546ed06f539faf4ed4d3d8f5
dfussell1 <daltonfussell1@gmail.com>
2024-02-10 19:40:25 -0600

    added data for seeding

---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO department (name)
+VALUES 
+    ("Management"),
+    ("Sales"),
+    ("Information Technology (IT)"),
+    ("Finance");
+
+INSERT INTO role (title, salary, department_id)
+VALUES 
+    ("CEO", 150000, 1),
+    ("Building Supervisor", 120000, 1),
+    ("Sales Manager", 90000, 2),
+    ("Sales Associate", 75000, 2)
+    ("IT Manager", 100000, 3),
+    ("IT Associate", 80000, 3),
+    ("Account Manager", 105000, 4),
+    ("Accountant", 85000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ("Cee", "Ayeo", 1, NULL),
+    ("James", "Better", 2, 1),
+    ("Eric", "Cartman", 3, NULL),
+    ("Ben", "Tin", 4, 2),
+    ("Michael", "Long", 5, NULL),
+    ("Mallory", "Keeps", 6, 3),
+    ("Meredith", "Ray", 7, NULL),
+    ("Sarah", "Thomas", 8, 4);
